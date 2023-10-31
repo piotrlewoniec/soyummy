f00db3639ae9c22d1bf90336885ca1321da88046
Patryk Stefański <119447102+S4ter@users.noreply.github.com>
2023-10-31 19:00:40 +0100

    front footer done

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -3,7 +3,6 @@ import { Main } from './Main/main';
 import { WelcomePage } from '../pages/WellcomPage/WellcomePage';
 import { RegisterPage } from '../pages/RegisterPage/RegisterPage';
 import { SigninPage } from '../pages/SigninPage/SigninPage';
-import { Footer } from './Footer/Footer';
 import { NotFoundPage } from '../pages/NotFoundPage/NotFoundPage';
 import { ShoppingListPage } from '../pages/ShoppingListPage/ShoppingListPage';
 
@@ -15,7 +14,6 @@ export const App = () => {
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/signin" element={<SigninPage />} />
         <Route path="/main" element={<Main />} />
-        <Route path="/footer" element={<Footer />} />
         <Route path="/shopping-list" element={<ShoppingListPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
