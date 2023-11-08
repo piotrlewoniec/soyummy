781c442f9de039e8c7034786559c0426403003a1
piotrlewoniec <117780551+piotrlewoniec@users.noreply.github.com>
2023-11-08 08:38:00 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,7 +13,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
 <BrowserRouter basename="/soyummy">
- // <BrowserRouter>
     <Provider store={store}>
       <ThemeProvider>
         <App />
